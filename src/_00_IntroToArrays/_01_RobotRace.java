da8f77924d14588f68f5cd75ad853a4f4239c9be
Samuel Sharp <samuelsharp4@gmail.com>
2018-11-10 18:51:00 -0800

    Finish Robot Racers, still need to add sound

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -1,31 +1,33 @@
 package _00_IntroToArrays;
-
 import java.util.Random;
-
 import javax.swing.JOptionPane;
-
 import org.jointheleague.graphical.robot.Robot;
-
 public class _01_RobotRace {
-	// 1. make a main method
 	public static void main(String[] args) {
-		//2. create an array of 5 robots.
-		Robot[]
-		//3. use a for loop to initialize the robots.
-
-			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
-		//5. use another for loop to iterate through the array and make each robot move 
-	    //   a random amount less than 50.
-    	
-		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-		//7. declare that robot the winner and throw it a party!
-    	
-		//8. try different races with different amounts of robots.
-    	
-	    //9. make the robots race around a circular track.
-
-	
+		Robot rbts[] = new Robot[5];
+		for(int i = 0; i<rbts.length; i++) {
+			Random r = new Random();
+			rbts[i] = new Robot();
+			rbts[i].setSpeed(Integer.MAX_VALUE);
+			rbts[i].setX(0);
+			rbts[i].setY(500);
+			rbts[i].move(i*100);
+			rbts[i].turn(90);
+			rbts[i].setSpeed(100);
+			rbts[i].penDown();
+			rbts[i].setPenColor(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+		}	
+		boolean finished = false;
+		while(!finished) {
+			for(int i = 0; i<rbts.length; i++) {
+				Random r = new Random();
+				rbts[i].move(r.nextInt(50));
+				if(rbts[i].getX() >= 800) {
+					finished = true;
+					rbts[i].sparkle();
+					JOptionPane.showMessageDialog(null, "And we have a winner!");
+				}
+			}
+		}	
 }
 }
